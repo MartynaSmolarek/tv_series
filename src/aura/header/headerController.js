18f0d032d03ae8ad5fe670ba620b7446067c880e
Martyna Smolarek <halomartyna@gmail.com>
2018-08-09 15:44:41 +0200

    controller for header

--- src/aura/header/headerController.js
+++ src/aura/header/headerController.js
@@ -1,19 +1,20 @@
 /**
  * Created by martyna.smolarek on 08.08.2018.
  */
-({      // przycisk do dodawania nowej tv serie
-        addNew : function (cmp, event, helper) {
-            alert("You clicked: " + event.getSource().get("v.label"));
-        },
+({
+    // przycisk do dodawania nowej tv serie
+    addNew : function (cmp, event, helper) {
+         alert("You clicked: " + event.getSource().get("v.label"));
+    },
 
-        // pole do wyszukiwania
-        handleKeyUp: function (cmp, evt) {
-                var isEnterKey = evt.keyCode === 13;
-                if (isEnterKey) {
-                    var queryTerm = cmp.find('enter-search').get('v.value');
-                    alert('Searched for "' + queryTerm + '"!');
-                }
-        },
+    // pole do wyszukiwania
+    handleKeyUp: function (cmp, evt) {
+        var isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            var queryTerm = cmp.find('enter-search').get('v.value');
+            alert('Searched for "' + queryTerm + '"!');
+        }
+    },
 
         /*searchKeyChange: function(component, event) {
                 window.location.hash = event.target.value;
