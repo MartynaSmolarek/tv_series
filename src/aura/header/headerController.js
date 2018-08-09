0d97a2bed024ab5a776f79bacac712aa429061ef
Martyna Smolarek <halomartyna@gmail.com>
2018-08-09 08:38:52 +0200

    controler for header, have a button add and searchable field

---
+++ src/aura/header/headerController.js
@@ -0,0 +1,30 @@
+/**
+ * Created by martyna.smolarek on 08.08.2018.
+ */
+({      // przycisk do dodawania nowej tv serie
+        addNew : function (cmp, event, helper) {
+            alert("You clicked: " + event.getSource().get("v.label"));
+        },
+
+        // pole do wyszukiwania
+        handleKeyUp: function (cmp, evt) {
+                var isEnterKey = evt.keyCode === 13;
+                if (isEnterKey) {
+                    var queryTerm = cmp.find('enter-search').get('v.value');
+                    alert('Searched for "' + queryTerm + '"!');
+                }
+        },
+
+        /*searchKeyChange: function(component, event) {
+                window.location.hash = event.target.value;
+        },
+
+        locationChange: function(component, event) {
+                var token = event.getParam("token") || "";
+        		component.set("v.searchKey", token);
+        },
+
+        clearText: function(component) {
+                window.location.hash = "";
+        }*/
+})
