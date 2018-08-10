0524cc41a2eaa51eabec32de1a3328964f43a4c6
Martyna Smolarek <halomartyna@gmail.com>
2018-08-10 15:07:15 +0200

    helper for serie

--- src/aura/Serie/SerieHelper.js
+++ src/aura/Serie/SerieHelper.js
@@ -2,5 +2,18 @@
  * Created by martyna.smolarek on 08.08.2018.
  */
 ({
+    getSeason: function(component) {
+        var action2 = component.get('c.getSeasons');
+        action2.setParam("tv_serie_id", component.get("v.serie").Id);
+        console.log("jestem w seasonsGet() w seasonContoller.js");
 
+        action2.setCallback (this, function(response) {
+            //var name = component.get("v.numOfSeason")
+            component.set("v.numOfSeason", response.getReturnValue());
+            var value = response.getReturnValue();
+
+        });
+        $A.enqueueAction(action2);
+
+    },
 })
